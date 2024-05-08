520b6b98bee59c851a47d2add7fdeed08b117989
Charly <carlosgarita3110@gmail.com>
2024-05-08 16:08:35 -0600

    feat: add registerProveedor endpoint to ProveedorController

--- api_facturas/src/main/java/com/api_facturas/Proveedores/controller/ProveedorController.java
+++ api_facturas/src/main/java/com/api_facturas/Proveedores/controller/ProveedorController.java
@@ -1,11 +1,12 @@
 package com.api_facturas.Proveedores.controller;
 
+import com.api_facturas.Proveedores.model.ProveedorEntity;
 import com.api_facturas.Proveedores.service.ProveedorService;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RestController
@@ -20,5 +21,16 @@ public class ProveedorController {
         this.proveedorService = proveedorService;
         this.httpSession = httpSession;
     }
-    
+
+    @PostMapping("/register")
+    public ResponseEntity<Object> registerProveedor(@Valid @RequestBody ProveedorEntity proveedorEntity) {
+        try {
+            ProveedorEntity registeredProveedor = proveedorService.registerProveedor(proveedorEntity);
+            return ResponseEntity.status(HttpStatus.CREATED).body(registeredProveedor);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+
 }
