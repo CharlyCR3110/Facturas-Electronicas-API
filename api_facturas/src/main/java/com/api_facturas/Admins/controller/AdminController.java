de56a1a7f52adf7093331dcafa3e55df47ffa68b
Charly <carlosgarita3110@gmail.com>
2024-05-08 18:22:51 -0600

    feat: Add AdminController to handle admin-related API endpoints

---
+++ api_facturas/src/main/java/com/api_facturas/Admins/controller/AdminController.java
@@ -0,0 +1,33 @@
+package com.api_facturas.Admins.controller;
+
+
+import com.api_facturas.Admins.model.AdminEntity;
+import com.api_facturas.Admins.service.AdminService;
+import com.api_facturas.Proveedores.model.ProveedorEntity;
+import com.api_facturas.Proveedores.service.ProveedorService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+@RestController
+@RequestMapping("/api/admins")
+public class AdminController {
+    private final AdminService adminService;
+    private final ProveedorService proveedorService;
+    final HttpSession httpSession;
+
+    public AdminController(AdminService adminService, ProveedorService proveedorService, HttpSession httpSession) {
+        this.adminService = adminService;
+        this.proveedorService = proveedorService;
+        this.httpSession = httpSession;
+    }
+
+}
