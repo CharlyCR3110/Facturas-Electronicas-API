460474e33d1aefe08ba81ed13db11ea1148710ce
Charly <carlosgarita3110@gmail.com>
2024-05-11 14:17:07 -0600

    feat: add AuthSection component for authentication functionality

--- client_facturas/src/App.jsx
+++ client_facturas/src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css'
+import AuthSection from './views/auth/AuthSection'
 
 function App () {
   return (
-    <>
-
-    </>
+    <div className='App'>
+      <AuthSection />
+    </div>
   )
 }
 
