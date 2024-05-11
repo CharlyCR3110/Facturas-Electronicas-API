4735ce519cc302b64078af8fdbb07cfa77a4514b
Charly <carlosgarita3110@gmail.com>
2024-05-11 11:24:32 -0600

    feat: configure web security and add password encoder
    This commit adds the WebSecurityConfig class to configure the security of the application. It includes authorization rules for different endpoints and sets up the authentication entry point for unauthorized requests. Additionally, it disables CSRF protection. The commit also adds a PasswordEncoder bean to use the BCryptPasswordEncoder for password hashing.

---
+++ api_facturas/src/main/java/com/api_facturas/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.api_facturas;
+
+// para configurar la seguridad de la aplicación
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .requestMatchers("/api/auth/logout").authenticated()
+                        .requestMatchers("/api/admins/**").hasAnyAuthority("admin")
+                        .requestMatchers("/api/**").hasAnyAuthority("proveedor")
+                )
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
