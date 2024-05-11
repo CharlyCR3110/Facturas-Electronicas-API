e5e7da8f3a46d7ae111cf24f51395eaae7e4dd51
Charly <carlosgarita3110@gmail.com>
2024-05-11 15:06:15 -0600

    refactor: remove unused links in AuthSection component

--- client_facturas/src/views/auth/AuthSection.jsx
+++ client_facturas/src/views/auth/AuthSection.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LoginSection from './LoginSection'
 import RegisterSection from './RegisterSection'
 
-const AuthSection = () => {
+const AuthSection = ({ setLoggedUser }) => {
   return (
     <BrowserRouter>
-      <Link to='/login' />
-      <Link to='/register' />
       <Routes>
-        <Route path='/login' element={<LoginSection sectionName='Iniciar Sesión' />} />
+        <Route path='/login' element={<LoginSection sectionName='Iniciar Sesión' setLoggedUser={setLoggedUser} />} />
         <Route path='/register' element={<RegisterSection sectionName='Registrarse' />} />
       </Routes>
     </BrowserRouter>
