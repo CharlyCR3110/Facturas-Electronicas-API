6fa44359e93439373b0b8b6bb2db0e4a45d963fd
Charly <carlosgarita3110@gmail.com>
2024-05-11 11:23:54 -0600

    feat: add findAllByRol method to UsuarioRepository

--- api_facturas/src/main/java/com/api_facturas/Usuarios/repository/UsuarioRepository.java
+++ api_facturas/src/main/java/com/api_facturas/Usuarios/repository/UsuarioRepository.java
@@ -3,10 +3,13 @@ package com.api_facturas.Usuarios.repository;
 import com.api_facturas.Usuarios.model.UsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Integer> {
     Optional<UsuarioEntity> findByCorreoAndContrasena (String correo, String contrasena);
 
     Optional<UsuarioEntity> findByCorreo(String correo);
+
+    List<UsuarioEntity> findAllByRol(String rol);
 }
