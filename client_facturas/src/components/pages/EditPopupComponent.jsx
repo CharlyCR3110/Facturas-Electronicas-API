1f09cb8838294b10270711e3eb7acf92006dc642
Charly <carlosgarita3110@gmail.com>
2024-05-13 17:57:24 -0600

    feat: add EditPopupComponent for editing data
    the code changes introduce a new file `EditPopupComponent.jsx` that implements a popup component for editing data. This component is used to display a form with input fields for each field in the `fields` prop. When the "Actualizar" button is clicked, the form data is collected and passed to the `onSubmit` function along with the `currentElementId`. The purpose of this commit is to add the functionality for editing data in the application.

---
+++ client_facturas/src/components/pages/EditPopupComponent.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const EditPopupComponent = ({ title, fields, onSubmit, handleClosePopup, setErrorMessage, currentElementId, setUpdatedElements }) => {
+  return (
+    <div className='edit-popup'>
+      <div className='popup-content'>
+        <div className='popup-header'>
+          <h2>{title}</h2>
+        </div>
+        <div className='popup-body'>
+          <form>
+            {fields.map(field => (
+              <div className='form-group' key={field.name}>
+                <label htmlFor={`${field.name}-edit`}>{field.label}</label>
+                <input
+                  type={field.type}
+                  name={field.name}
+                  id={`${field.name}-edit`}
+                  defaultValue={field.value}
+                  required
+                />
+              </div>
+            ))}
+          </form>
+          <button
+            className='action-btn' onClick={() => {
+              const formData = fields.reduce((acc, field) => ({ ...acc, [field.name]: document.getElementById(`${field.name}-edit`).value }), {})
+              onSubmit(currentElementId, formData, setErrorMessage, setUpdatedElements, handleClosePopup)
+            }}
+          >Actualizar
+          </button>
+          <button className='cancel-btn' onClick={handleClosePopup}>Cancelar</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default EditPopupComponent
