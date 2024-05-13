ab910723a3573f24dd910ff90aa4458a84f5afc3
Charly <carlosgarita3110@gmail.com>
2024-05-13 11:32:27 -0600

    refactor: update MainRouter to use AccountInfoSection instead of AccountInfoContainer

--- client_facturas/src/views/main/MainRouter.jsx
+++ client_facturas/src/views/main/MainRouter.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import AccountInfoContainer from '../account_info/AccountInfoContainer'
+import AccountInfoSection from '../account_info/AccountInfoSection'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const MainRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/account_info' element={<AccountInfoContainer />} />
+        <Route path='/account_info' element={<AccountInfoSection />} />
       </Routes>
     </BrowserRouter>
   )
