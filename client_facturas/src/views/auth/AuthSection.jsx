1e558df06cf4100b2d06593be361372d663db4a2
Charly <carlosgarita3110@gmail.com>
2024-05-12 20:08:11 -0600

    refactor: remove unnecessary prop from AuthSection component

--- client_facturas/src/views/auth/AuthSection.jsx
+++ client_facturas/src/views/auth/AuthSection.jsx
@@ -3,11 +3,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LoginSection from './LoginSection'
 import RegisterSection from './RegisterSection'
 
-const AuthSection = ({ setLoggedUser }) => {
+const AuthSection = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<LoginSection sectionName='Iniciar Sesión' setLoggedUser={setLoggedUser} />} />
+        <Route path='/login' element={<LoginSection sectionName='Iniciar Sesión' />} />
         <Route path='/register' element={<RegisterSection sectionName='Registrarse' />} />
       </Routes>
     </BrowserRouter>
