dd0e98ef7a897e15f6613fbf9734f4a3b5e5d7a7
Charly <carlosgarita3110@gmail.com>
2024-05-13 10:14:59 -0600

    refactor: update PopupComponent prop name to 'message'

--- client_facturas/src/components/popups/PopupComponent.jsx
+++ client_facturas/src/components/popups/PopupComponent.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import '../../assets/css/errors/popup.css'
 
-const PopupComponent = ({ error, onClose, type }) => {
+const PopupComponent = ({ messsage, onClose, type }) => {
   const popupClass = type === 'confirmation' ? 'confirmation' : 'error'
 
   return (
     <div className='overlay'>
       <div className={`popup ${popupClass}`}>
         <label htmlFor='toggle' className='close-btn' onClick={onClose}>&times;</label>
-        <p className='message'>{error}</p>
+        <p className='message'>{messsage}</p>
       </div>
     </div>
   )
