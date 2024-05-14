d4af438c772a362888b7d47d48e786f4ba259502
Charly <carlosgarita3110@gmail.com>
2024-05-14 12:15:07 -0600

    feat: Add conditional rendering for edit functionality
    - Added conditional rendering to display different buttons based on the type of operation (edit or expand).
    - Updated TableComponent to accept a new prop `handleExport` for exporting functionality.
    - Extracted the logic to check if edit functionality is available into a separate variable `isEditAvailable`.
    - Adjusted the rendering of the popup component based on the value of `isEditAvailable`.

--- client_facturas/src/components/pages/TableComponent.jsx
+++ client_facturas/src/components/pages/TableComponent.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import EditAddPopupComponent from './EditAddPopupComponent'
 
-const TableComponent = ({ headers, data, handleEdit, handleDelete, handleSendToInvoice, setErrorMessage, popupTitle, popupFields, setCurrentElement, setElements }) => {
+const TableComponent = ({ headers, data, handleEdit, handleDelete, handleSendToInvoice, setErrorMessage, popupTitle, popupFields, setCurrentElement, setElements, handleExport }) => {
   const [showPopup, setShowPopup] = useState(false)
   const [currentElementId, setCurrentElementId] = useState('')
 
@@ -10,7 +10,10 @@ const TableComponent = ({ headers, data, handleEdit, handleDelete, handleSendToI
     setCurrentElementId(row[0])
     setShowPopup(true)
   }
+
   const handleClosePopup = () => { setShowPopup(false) }
+
+  const isEditAvailable = popupTitle.includes('Editar')
 
   return (
     <>
@@ -29,26 +32,50 @@ const TableComponent = ({ headers, data, handleEdit, handleDelete, handleSendToI
                 <td key={cellIndex}>{cell}</td>
               ))}
               <td>
-                <button id='edit_button' className='toggle-popup toggle-edit-popup action-btn' onClick={() => handleShowPopup(row)}>Editar</button>
-                <button id='delete_button' className='action-btn' onClick={() => handleDelete(row, setErrorMessage, setElements)}>Eliminar</button>
-                <button id='to_invoice_button' className='action-btn' onClick={() => handleSendToInvoice(row, setErrorMessage)}>Enviar a Factura</button>
+                {isEditAvailable
+                  ? (
+                    <>
+                      <button id='edit_button' className='toggle-popup toggle-edit-popup action-btn' onClick={() => handleShowPopup(row)}>Editar</button>
+                      <button id='delete_button' className='action-btn' onClick={() => handleDelete(row, setErrorMessage, setElements)}>Eliminar</button>
+                      <button id='to_invoice_button' className='action-btn' onClick={() => handleSendToInvoice(row, setErrorMessage)}>Enviar a Factura</button>
+                    </>
+                    )
+                  : (
+                    <>
+                      <button id='expand_button' className='toggle-popup toggle-edit-popup action-btn' onClick={() => handleShowPopup(row)}>Expandir</button>
+                      <button id='delete_button' className='action-btn' onClick={() => handleDelete(row, setErrorMessage, setElements)}>Eliminar</button>
+                      <button id='export_button' className='action-btn' onClick={() => handleExport(row, setErrorMessage)}>Exportar</button>
+                    </>
+                    )}
               </td>
             </tr>
           ))}
         </tbody>
       </table>
 
-      {showPopup && (
-        <EditAddPopupComponent
-          title={popupTitle}
-          fields={popupFields}
-          onSubmit={handleEdit}
-          handleClosePopup={handleClosePopup}
-          setErrorMessage={setErrorMessage}
-          currentElementId={currentElementId}
-          setUpdatedElements={setElements}
-        />
-      )}
+      {isEditAvailable
+        ? (
+            showPopup
+              ? (
+                <EditAddPopupComponent
+                  title={popupTitle}
+                  fields={popupFields}
+                  onSubmit={handleEdit}
+                  handleClosePopup={handleClosePopup}
+                  setErrorMessage={setErrorMessage}
+                  currentElementId={currentElementId}
+                  setUpdatedElements={setElements}
+                />
+                )
+              : null
+          )
+        : (
+            showPopup
+              ? (
+                <p>WORKING ON</p>
+                )
+              : null
+          )}
     </>
   )
 }
