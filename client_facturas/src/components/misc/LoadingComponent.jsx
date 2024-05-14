470af944035979d12c73b1ceadb9b98054e61890
Charly <carlosgarita3110@gmail.com>
2024-05-14 11:47:48 -0600

    feat: add LoadingComponent for displaying loading state

---
+++ client_facturas/src/components/misc/LoadingComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../assets/css/misc/loading.css'
+
+const LoadingComponent = () => {
+  return (
+    <div class='loading'>Loading&#8230;</div>
+  )
+}
+
+export default LoadingComponent
