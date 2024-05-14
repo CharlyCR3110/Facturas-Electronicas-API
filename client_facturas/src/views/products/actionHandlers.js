0edd55619806f4bc57dc106294517ccb6667adfd
Charly <carlosgarita3110@gmail.com>
2024-05-14 10:48:10 -0600

    feat: add handleSendToInvoice function for sending product to invoice
    This commit adds the `handleSendToInvoice` function to the `actionHandlers.js` file. The function allows sending a product to the invoice creator by making a POST request to the `/api/invoices/sendProductToInvoiceCreator` endpoint. It retrieves the `idProducto` from the provided row and sends it in the request payload. If the request is successful, the product is added to the invoice and the updated list of products is stored in the session. If there is an error, an error message is displayed. This enhancement improves the functionality of the application by enabling users to easily add products to the invoice.

--- client_facturas/src/views/products/actionHandlers.js
+++ client_facturas/src/views/products/actionHandlers.js
@@ -119,3 +119,28 @@ export const handleSearch = async (searchValue, setErrorMessage, setUpdatedEleme
     setErrorMessage('Error al buscar productos')
   }
 }
+
+export const handleSendToInvoice = async (row, setErrorMessage) => {
+  const idProducto = row[0]
+  try {
+    const response = await fetch(`http://localhost:8080/api/invoices/sendProductToInvoiceCreator?idProducto=${idProducto}`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    if (!response.ok) {
+      throw new Error('Error al enviar el producto a la factura')
+    }
+
+    console.log('Producto enviado a la factura correctamente')
+    const data = await response.json()
+    console.log(data)
+    window.sessionStorage.setItem('onInvoiceProducts', JSON.stringify(data))
+  } catch (error) {
+    console.error('Error al enviar el producto a la factura:', error.message)
+    setErrorMessage(`Error al enviar el producto con id ${idProducto} a la factura`)
+  }
+}
