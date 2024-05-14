a9a7e4dd0f3b067ebafa5900bca7eaf38bfecd34
Charly <carlosgarita3110@gmail.com>
2024-05-13 21:05:56 -0600

    feat: add delete client functionality
    The code changes in `actionHandlers.js` introduce a new function `handleDelete` that sends a DELETE request to the server to delete a client. This commit adds the functionality to delete a client in the application.

--- client_facturas/src/views/clients/actionHandlers.js
+++ client_facturas/src/views/clients/actionHandlers.js
@@ -1,3 +1,27 @@
+export const handleDelete = (row, setErrorMessage, setUpdatedElements) => {
+  const idCliente = row[0]
+  fetch(`http://localhost:8080/api/clients/delete/${idCliente}`, {
+    method: 'DELETE',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error al eliminar el cliente')
+      }
+
+      fetchUpdatedClients(setUpdatedElements)
+
+      console.log('Cliente eliminado correctamente')
+    })
+    .catch(error => {
+      console.error('Error al eliminar el cliente:', error.message)
+      setErrorMessage(`Error al eliminar el cliente con id ${idCliente}`)
+    })
+}
+
 const fetchUpdatedClients = async (setClients) => {
   try {
     const response = await fetch('http://localhost:8080/api/clients/', {
