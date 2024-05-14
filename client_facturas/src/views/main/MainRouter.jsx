4ecb3657387cebe6a5a19fea07fba74bd9e58628
Charly <carlosgarita3110@gmail.com>
2024-05-14 14:15:35 -0600

    feat: Add InvoicesSection to MainRouter
    The code changes in `MainRouter.jsx` add the `InvoicesSection` component to the router configuration, allowing the `/invoices` route to render the invoices section of the application. This change enables providers to navigate to the invoices page and view their invoices.

--- client_facturas/src/views/main/MainRouter.jsx
+++ client_facturas/src/views/main/MainRouter.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import AccountInfoSection from '../account_info/AccountInfoSection'
 import ProductsSection from '../products/ProductsSection'
 import ClientsSections from '../clients/ClientsSection'
+import InvoicesSection from '../invoices/InvoicesSection'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const MainRouter = () => {
@@ -11,6 +12,7 @@ const MainRouter = () => {
         <Route path='/account_info' element={<AccountInfoSection />} />
         <Route path='/clients' element={<ClientsSections />} />
         <Route path='/products' element={<ProductsSection />} />
+        <Route path='/invoices' element={<InvoicesSection />} />
       </Routes>
     </BrowserRouter>
   )
