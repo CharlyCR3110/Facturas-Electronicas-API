7eda55a53964fda6c30460c341e318efcd3ef9e8
Charly <carlosgarita3110@gmail.com>
2024-05-14 19:08:57 -0600

    feat: add InvoiceCreatorSection for creating new invoices
    This commit adds the InvoiceCreatorSection component to the MainRouter. It allows users to create new invoices by navigating to the '/invoice_creator' route. The InvoiceCreatorSection component handles the form submission and validation for creating invoices. This functionality enables users to easily create and manage invoices within the application.

--- client_facturas/src/views/main/MainRouter.jsx
+++ client_facturas/src/views/main/MainRouter.jsx
@@ -3,6 +3,7 @@ import AccountInfoSection from '../account_info/AccountInfoSection'
 import ProductsSection from '../products/ProductsSection'
 import ClientsSections from '../clients/ClientsSection'
 import InvoicesSection from '../invoices/InvoicesSection'
+import InvoiceCreatorSection from '../invoice_creator/InvoiceCreatorSection'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const MainRouter = () => {
@@ -13,6 +14,7 @@ const MainRouter = () => {
         <Route path='/clients' element={<ClientsSections />} />
         <Route path='/products' element={<ProductsSection />} />
         <Route path='/invoices' element={<InvoicesSection />} />
+        <Route path='/invoice_creator' element={<InvoiceCreatorSection />} />
       </Routes>
     </BrowserRouter>
   )
