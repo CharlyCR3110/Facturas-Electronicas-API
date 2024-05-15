be79b0c1e20d3f96022776513a159e0bb671657e
Charly <carlosgarita3110@gmail.com>
2024-05-15 17:50:33 -0600

    feat: add handleChangeStatus function to handle provider state change API call

--- client_facturas/src/views/admin/actionHandlers.js
+++ client_facturas/src/views/admin/actionHandlers.js
@@ -19,3 +19,24 @@ export const fetchProviders = async (setProvidersList) => {
     console.error(error)
   }
 }
+
+export const handleChangeStatus = async (row, setElements) => {
+  try {
+    const response = await fetch(`http://localhost:8080/api/admins/changeProviderState?idProvider=${row[0]}`, {
+      method: 'PATCH',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    if (!response.ok) {
+      throw new Error('Error al cambiar el estado del proveedor')
+    }
+
+    const data = await response.json()
+    setElements(data)
+  } catch (error) {
+    console.error(error)
+  }
+}
