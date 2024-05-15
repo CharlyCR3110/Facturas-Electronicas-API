6301d8b8de12f05f66d6ef6b21084c452d9c4b6c
Charly <carlosgarita3110@gmail.com>
2024-05-15 10:56:11 -0600

    feat: add handleAddClientToInvoice function for adding a client to an invoice
    The code changes in `actionHandlers.js` add a new function `handleAddClientToInvoice` that allows adding a client to an invoice. This function makes a POST request to the `/api/invoices/selectClient` endpoint with the client identification as a parameter.

---
+++ client_facturas/src/views/invoice_creator/actionHandlers.js
@@ -0,0 +1,21 @@
+export const handleAddClientToInvoice = async (clientIdentification, setClient, setErrorMessage) => {
+  try {
+    const response = await fetch(`http://localhost:8080/api/invoices/selectClient?client=${clientIdentification}`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    if (!response.ok) {
+      throw new Error('Error al agregar el cliente a la factura')
+    }
+
+    const data = await response.json()
+    console.log(data)
+    setClient(data)
+  } catch (error) {
+    setErrorMessage(`Error al agregar el cliente con id ${clientIdentification} a la factura`)
+  }
+}
