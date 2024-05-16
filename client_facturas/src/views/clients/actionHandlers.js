34d66c067a918b00018e9034c4df0abda014c89c
Charly <carlosgarita3110@gmail.com>
2024-05-15 20:12:07 -0600

    feat: add fetchClients function to handle API call for fetching clients

--- client_facturas/src/views/clients/actionHandlers.js
+++ client_facturas/src/views/clients/actionHandlers.js
@@ -1,3 +1,26 @@
+export const fetchClients = async (setClients, setLoading) => {
+  try {
+    const response = await fetch('http://localhost:8080/api/clients/', {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    if (!response.ok) {
+      throw new Error('Error al obtener los clientes')
+    }
+
+    const data = await response.json()
+    setClients(data)
+    setLoading(false)
+  } catch (error) {
+    console.error('Error al obtener los clientes:', error.message)
+    setLoading(false)
+  }
+}
+
 export const handleDelete = (row, setErrorMessage, setUpdatedElements) => {
   const idCliente = row[0]
   fetch(`http://localhost:8080/api/clients/delete/${idCliente}`, {
