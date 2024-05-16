4228fa080e565c65a6f175ee28eebd801cb2061e
Charly <carlosgarita3110@gmail.com>
2024-05-16 14:18:43 -0600

    feat: add registration form submission handler
    The `onRegisterSubmit` function is added to handle the form submission for the registration page. It sends a POST request to the API endpoint for user registration and handles success and error responses accordingly.

--- client_facturas/src/views/auth/formsHandler.js
+++ client_facturas/src/views/auth/formsHandler.js
@@ -1,6 +1,8 @@
 const API_URL_LOGIN = 'http://localhost:8080/api/auth/login'
+const API_URL_REGISTER = 'http://localhost:8080/api/auth/register'
 
-export const onLoginSubmit = async (event, formData, setErrorMessage) => {
+// se agregan los parametros setFormData y setIsRegisterSuccess, pero no se utilizan solo se agregan para que no haya errores
+export const onLoginSubmit = async (event, formData, setErrorMessage, setFormData, setIsRegisterSuccess) => {
   event.preventDefault()
   try {
     const response = await fetch(API_URL_LOGIN, {
@@ -28,6 +30,38 @@ export const onLoginSubmit = async (event, formData, setErrorMessage) => {
     }
   } catch (error) {
     console.error('Error al iniciar sesión:', error.message)
+    setErrorMessage(error.message)
+  }
+}
+
+export const onRegisterSubmit = async (event, formData, setErrorMessage, setFormData, setIsRegisterSuccess) => {
+  event.preventDefault()
+  try {
+    const response = await fetch(API_URL_REGISTER, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+
+    if (response.status === 409) {
+      throw new Error('El correo ya está registrado')
+    } else if (!response.ok) {
+      throw new Error('Error al registrar el usuario')
+    }
+
+    const user = await response.json()
+
+    console.log('Usuario registrado', user)
+
+    // Limpiar el formulario
+    setFormData({})
+
+    // Mostrar mensaje de éxito
+    setIsRegisterSuccess(true)
+  } catch (error) {
+    console.error('Error al registrar el usuario:', error.message)
     setErrorMessage(error.message)
   }
 }
