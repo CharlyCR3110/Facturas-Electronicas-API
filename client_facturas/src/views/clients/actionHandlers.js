3adf6d136defc3236e3879b6cdd71be327bfbdd8
Charly <carlosgarita3110@gmail.com>
2024-05-15 20:20:19 -0600

    refactor: delete `fetchUpdatedClients` method, instead, `fetchClients` method is used

--- client_facturas/src/views/clients/actionHandlers.js
+++ client_facturas/src/views/clients/actionHandlers.js
@@ -39,32 +39,11 @@ export const handleDelete = (row, setErrorMessage, setUpdatedElements) => {
         throw new Error(errorMessage || 'Error al eliminar el cliente')
       }
 
-      fetchUpdatedClients(setUpdatedElements)
+      fetchClients(setUpdatedElements, null)
     })
     .catch(error => {
       setErrorMessage(error.message)
     })
-}
-
-const fetchUpdatedClients = async (setClients) => {
-  try {
-    const response = await fetch('http://localhost:8080/api/clients/', {
-      method: 'GET',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-
-    if (!response.ok) {
-      throw new Error('Error al obtener los clientes')
-    }
-
-    const data = await response.json()
-    setClients(data)
-  } catch (error) {
-    console.error('Error al obtener los clientes:', error.message)
-  }
 }
 
 export const handleEdit = async (currentElementId, formData, setErrorMessage, setUpdatedElements, handleClosePopup) => {
@@ -87,7 +66,7 @@ export const handleEdit = async (currentElementId, formData, setErrorMessage, se
 
     console.log('Cliente editado correctamente')
 
-    fetchUpdatedClients(setUpdatedElements)
+    fetchClients(setUpdatedElements, null)
 
     handleClosePopup()
   } catch (error) {
@@ -113,7 +92,7 @@ export const handleAdd = async (currentElementId, formData, setErrorMessage, set
       throw new Error(errorMessage || 'Error al agregar el cliente')
     }
 
-    fetchUpdatedClients(setUpdatedElements)
+    fetchClients(setUpdatedElements, null)
 
     handleClosePopup()
   } catch (error) {
